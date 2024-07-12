ab54dcdabad6524926149f1a384548b4f0aa0b3a
Tester <testing.dev004@gmail.com>
2024-07-12 22:41:09 +0530

    Shared on GIT first program

---
+++ src/FirstProgram/HelloProgram.java
@@ -0,0 +1,12 @@
+package FirstProgram;
+
+public class HelloProgram {
+
+    public static void main(String bajaj []) {
+
+        System.out.println("Hello");
+    }
+
+
+
+}
---
+++ src/FirstProgram/NewProgram.java
@@ -0,0 +1,8 @@
+package FirstProgram;
+
+public class NewProgram {
+
+    public static void main(String[] args) {
+
+    }
+}
