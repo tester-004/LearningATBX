6a96cf5676ecb1d88ba81850e08f8eab13ed8f3a
Tester <testing.dev004@gmail.com>
2024-07-12 22:46:49 +0530

    Shared on GIT first program

--- src/FirstProgram/NewProgram.java
+++ src/FirstProgram/NewProgram.java
@@ -5,6 +5,6 @@ public class NewProgram {
     public static void main(String[] args) {
 
         System.out.println(2+2);
-        System.out.println(2-2);
+        System.out.println(2+2);
     }
 }
