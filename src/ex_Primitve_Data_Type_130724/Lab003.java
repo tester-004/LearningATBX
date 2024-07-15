f736ffd02c9045e8d907d9deed068f5ddf9eaafb
Tester <testing.dev004@gmail.com>
2024-07-15 22:37:21 +0530

    Shared on GIT first program

---
+++ src/ex_Primitve_Data_Type_130724/Lab003.java
@@ -0,0 +1,27 @@
+package ex_Primitve_Data_Type_130724;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        boolean H = true;
+        boolean i = 5<3;
+        byte age1 = 122;
+        int a = 12;
+        char b = 'A';
+        char c = 'b';
+        short d = 32767;
+        long e = 123456787;
+        double f = 5234567890123345567779889899898989898989898d;
+        float g = 3.14f;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(age1);
+        System.out.println(H);
+        System.out.println(i);
+    }
+}
---
+++ src/ex_Primitve_Data_Type_130724/Lab004.java
@@ -0,0 +1,14 @@
+package ex_Primitve_Data_Type_130724;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        byte a = 10;
+        System.out.println(a);
+       // int age = 122; // its a bigger container and byte takes -127 to 128. If we will take int memory loss.
+        byte age1 = 122;
+
+        System.out.println(age1);
+
+
+    }
+}
